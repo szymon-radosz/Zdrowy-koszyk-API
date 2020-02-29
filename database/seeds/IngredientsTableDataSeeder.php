9d4f4552505f9db7a1981ca5a71c042582e0c6ed
Szymon Radosz <szymonradosz@Szymons-MacBook-Air.local>
2020-02-29 17:53:43 +0100

    added more ingredients to test

--- database/seeds/IngredientsTableDataSeeder.php
+++ database/seeds/IngredientsTableDataSeeder.php
@@ -32,8 +32,7 @@ class IngredientsTableDataSeeder extends Seeder
             [
                 "name" => "Skrobia kukurydziana modyfikowana ",
                 "keywords" => "skrobia kukurydziana modyfikowana",
-                "description" => "Alergia na skrobię nie ma prawa występować. Reakcje alergiczne powodują białka, natomiast skrobia jest wielocukrem. Trzeba jednak pamiętać, że skrobia jest węglowodanem, których nadmiar w diecie jest niepożądany z wielu przyczyn. IG skrobi modyfikowanej jest równy 100, co mówi o tym, że po jej spożyciu następuje gwałtowny wyrzut glukozy do krwi.
-                                    Czy skrobia jest zdrowa? Nie mniej zdrowa i nie mniej szkodliwa niż inne węglowodany. Chyba że mówimy o skrobi opornej, która jest prebiotykiem. Wtedy wykazuje ona pozytywne działanie zdrowotne. O szkodliwość E1422 nie trzeba się martwić. Jednak dokonując wyborów żywieniowych, warto się zastanowić, czy kupowanie kiełbasy, która ma tak słaby skład, że musi być sklejana skrobią, ma sens. Bowiem najczęściej skrobia spożywcza jest używana w żywności o bardzo niskiej jakości",
+                "description" => "Alergia na skrobię nie ma prawa występować. Reakcje alergiczne powodują białka, natomiast skrobia jest wielocukrem. Trzeba jednak pamiętać, że skrobia jest węglowodanem, których nadmiar w diecie jest niepożądany z wielu przyczyn. IG skrobi modyfikowanej jest równy 100, co mówi o tym, że po jej spożyciu następuje gwałtowny wyrzut glukozy do krwi. <br />Czy skrobia jest zdrowa? Nie mniej zdrowa i nie mniej szkodliwa niż inne węglowodany. Chyba że mówimy o skrobi opornej, która jest prebiotykiem. Wtedy wykazuje ona pozytywne działanie zdrowotne. O szkodliwość E1422 nie trzeba się martwić. Jednak dokonując wyborów żywieniowych, warto się zastanowić, czy kupowanie kiełbasy, która ma tak słaby skład, że musi być sklejana skrobią, ma sens. Bowiem najczęściej skrobia spożywcza jest używana w żywności o bardzo niskiej jakości",
                 "wiki_url" => "https://wylecz.to/zywnosc/skrobia-modyfikowana-e1422-wlasciwosci-zastosowanie-szkodliwosc/",
                 "wiki_title" => "wylecz.to",
                 "type" => "bad"
@@ -41,15 +40,16 @@ class IngredientsTableDataSeeder extends Seeder
             [
                 "name" => "Syrop glukozowo-fruktozowy",
                 "keywords" => "syrop glukozowy,Syrop glukozowo-fruktozowy",
-                "description" => "Niekorzystne oddziaływanie syropu na zdrowie człowieka obserwuje się najczęściej przy jego udziale w diecie pomiędzy 35 a 65%[5]. Dotąd ustalono, że spożywanie dużych ilości produktów zawierających syrop glukozowo-fruktozowy:
-                                    powoduje szybkie tycie i chroniczną otyłość (fruktoza spożywana nawet w niewielkich ilościach, nastawia organizm na produkcję tkanki tłuszczowej, zwłaszcza gromadzącej się wokół narządów wewnętrznych). Według prof. Grażyny Cichosz, żaden inny składnik diety nie sprzyja otyłości tak, jak fruktoza,
-                                    zwiększa ryzyko wystąpienia cukrzycy typu 2, potęguje apetyt, prowadzi do reaktywnej hipoglikemii (gwałtownych zmian poziomu insuliny oraz glukozy we krwi),
-                                    podwyższa poziom lipoproteiny niskiej gęstości i trójglicerydów, powoduje spadek wrażliwości organizmu na leptynę,
-                                    hamuje wytwarzanie ATP,
-                                    przyczynia się do zwiększenia możliwości wystąpienia zespołu jelita drażliwego,
-                                    sprzyja rozwojowi nowotworu jelita grubego u myszy[21]
-                                    może powodować zaćmę cukrzycową.",
+                "description" => "Niekorzystne oddziaływanie syropu na zdrowie człowieka obserwuje się najczęściej przy jego udziale w diecie pomiędzy 35 a 65%[5].<br /> Dotąd ustalono, że spożywanie dużych ilości produktów zawierających syrop glukozowo-fruktozowy: powoduje szybkie tycie i chroniczną otyłość (fruktoza spożywana nawet w niewielkich ilościach, nastawia organizm na produkcję tkanki tłuszczowej, zwłaszcza gromadzącej się wokół narządów wewnętrznych). Według prof. Grażyny Cichosz, żaden inny składnik diety nie sprzyja otyłości tak, jak fruktoza, zwiększa ryzyko wystąpienia cukrzycy typu 2, potęguje apetyt, prowadzi do reaktywnej hipoglikemii (gwałtownych zmian poziomu insuliny oraz glukozy we krwi), podwyższa poziom lipoproteiny niskiej gęstości i trójglicerydów, powoduje spadek wrażliwości organizmu na leptynę, hamuje wytwarzanie ATP, przyczynia się do zwiększenia możliwości wystąpienia zespołu jelita drażliwego, sprzyja rozwojowi nowotworu jelita grubego u myszy[21] <br />może powodować zaćmę cukrzycową.",
                 "wiki_url" => "https://pl.wikipedia.org/wiki/Syrop_glukozowo-fruktozowy",
+                "wiki_title" => "wikipedia",
+                "type" => "bad"
+            ],
+            [
+                "name" => "Olej palmowy",
+                "keywords" => "tłuszcz palmowy, olej palmowy",
+                "description" => "Olej palmowy używany jest najczęściej do smażenia, co powoduje jego stopniowe utlenianie się. Produkty utleniania oleju oraz innych tłuszczów uznawane są za szkodliwe dla zdrowia[5].<br/>Kwas palmitynowy według badań amerykańskiego Center for Science in the Public Interest (CSPI) spożywany w nadmiarze podnosi zawartość cholesterolu we krwi i może prowadzić do chorób serca[6]. Według raportu CSPI, Światowa Organizacja Zdrowia (WHO) oraz amerykański National Heart, Lung and Blood Institute (NHLBI) zalecają ograniczenie spożycia kwasu palmitynowego i produktów bogatych w tłuszcze nasycone. WHO wykazało związek spożycia kwasu palmitynowego (podobnie jak izomerów trans kwasów tłuszczowych) z chorobami układu krążenia[7].<br />Ponadto w maju 2016 roku Europejski Urząd ds. Bezpieczeństwa Żywności wydał raport, w którym stwierdzono, że olej palmowy (i inne tłuszcze palmowe) zawiera najwyższe wśród tłuszczów roślinnych stężenia szkodliwych, potencjalnie rakotwórczych chloropochodnych. Powstają one w procesie produkcji tłuszczu, zwłaszcza rafinacji oleju prowadzonej w temperaturze powyżej 200 °C[8].",
+                "wiki_url" => "https://pl.wikipedia.org/wiki/Olej_palmowy",
                 "wiki_title" => "wikipedia",
                 "type" => "bad"
             ]
