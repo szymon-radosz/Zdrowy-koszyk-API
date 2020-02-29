91826ef5db6911d76ec4c0ea7f927a1402ef3c1a
Szymon Radosz <szymonradosz@Szymons-MacBook-Air.local>
2020-02-29 12:27:19 +0100

    push to my devil

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,17 @@ Route::get('product/url/get', 'UrlController@index');
 
 Route::post('product/new', 'ProductController@store');
 
-Route::get('product/find/{barcode}', 'ProductController@find');
-
 Route::post('login', 'UserController@authenticate');
 Route::post('register', 'UserController@register');
 Route::post('checkIfEmailExists', 'UserController@checkIfEmailExists');
+
+Route::get('product/find/{barcode}', array('middleware' => 'cors', 'uses' => 'ProductController@find'));
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('user', 'UserController@getAuthenticatedUser');
     Route::get('get-products', 'ProductController@index');
     Route::post('update-product', 'ProductController@update');
     Route::post('remove-product', 'ProductController@remove');
-    
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
