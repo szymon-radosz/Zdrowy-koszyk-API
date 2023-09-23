8b512e3dc47713d268b521596148338f432a5b65
Simon <radoszszymon@gmail.com>
2023-09-23 09:24:20 +0200

    fix: cors na request z wyszukiwarka

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,7 @@ Route::post('register', 'UserController@register');
 Route::post('checkIfEmailExists', 'UserController@checkIfEmailExists');
 
 Route::get('product/find/{barcode}/{deviceId?}/{deviceBrand?}', array('middleware' => 'cors', 'uses' => 'ProductController@find'));
-// Route::get('product/search/{querySearch}', array('middleware' => 'cors', 'uses' => 'ProductController@findBySearchInput'));
-Route::get('product/search/{querySearch}', 'ProductController@findBySearchInput');
+Route::get('product/search/{querySearch}', array('middleware' => 'cors', 'uses' => 'ProductController@findBySearchInput'));
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('user', 'UserController@getAuthenticatedUser');
