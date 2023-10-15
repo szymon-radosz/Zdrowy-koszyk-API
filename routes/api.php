cd3b54cf625a6f9fb6bd4da6b59423a190e8a275
Simon <radoszszymon@gmail.com>
2023-10-15 08:59:02 +0200

    fix: api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 
+Route::get('product/search/{querySearch}', 'ProductController@findBySearchInput');
+
 Route::post('url/new', 'UrlController@store');
 Route::get('product/url/get', 'UrlController@index');
 
@@ -16,8 +18,8 @@ Route::post('checkIfEmailExists', 'UserController@checkIfEmailExists');
 // Route::get('product/find/{barcode}/{deviceId?}/{deviceBrand?}', array('middleware' => 'cors', 'uses' => 'ProductController@find'));
 // Route::get('product/search/{querySearch}', array('middleware' => 'cors', 'uses' => 'ProductController@findBySearchInput'));
 
-Route::get('product/find/{barcode}/{deviceId?}/{deviceBrand?}', 'ProductController@find');
-Route::get('product/search/{querySearch}', 'ProductController@findBySearchInput');
+// Route::get('product/find/{barcode}/{deviceId?}/{deviceBrand?}', 'ProductController@find');
+
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('user', 'UserController@getAuthenticatedUser');
